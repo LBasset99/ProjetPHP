c6c96b0107318eab67eac80f45e40cc41b06317f
BASSET Lucas <bassetlu@hostname.iut2.upmf-grenoble.fr>
2018-10-24 15:33:34 +0200

    i2

---
+++ view/panier.view.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="panier.view.css">
+    <title>Panier - Vlad Shop Unofficial</title>
+  </head>
+  <body>
+
+  </body>
+</html>
