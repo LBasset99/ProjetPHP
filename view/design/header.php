d5bc1d99de434411b512548cb9cc66a86cbe8e81
Lucas Basset <lbasset99@gmail.com>
2018-10-28 21:55:09 +0100

    1

---
+++ view/design/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="../header.css" type="text/css" rel="stylesheet" />
+  </head>
+  <header>
+    <ul>
+      <li><a href="index.php">Acceuil</a></li>
+      <li><a href="panier.view.php">Panier</a></li>
+      <li><a href="inscription.view.php">S'inscrire</a></li>
+      <li><a href="connexion.view.php">Se connecter</a></li>
+    </ul>
+  </header>
+</html>
