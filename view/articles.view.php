173566e269d4dd5cd68fceb08224f04306525959
Lucas <lbasset99@gmail.com>
2018-11-08 20:50:00 +0100

    Grille rendu

--- view/articles.view.php
+++ view/articles.view.php
@@ -13,7 +13,7 @@
     print('<img src="'.$url.$unArt->image.'" alt="article">');
     print('<h4>'.$unArt->libelle.'</h4>');
     print('<h6>&nbsp &nbsp &nbsp'.$unArt->prix.'€</h6>');
-    print('<button class="ajouter" type="button" name="button">AJOUTER AU PANIER</button>');
+    print('<a href="../controler/panier.ctrl.php?action=ajout&amp;l='.$unArt->ref.'&amp;q=1" class="ajouter">AJOUTER AU PANIER</a>');
     print('</div>');
     print('</a>');
     ?>
